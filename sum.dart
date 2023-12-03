761c5e890cd74cfbec6fa7bdb597cb1918b99737
Amrithesh S <94913836+amritheshsiva@users.noreply.github.com>
2023-12-03 23:01:52 +0530

    Create sum.dart

---
+++ sum.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+void main() {
+  stdout.write("Enter number 1: ");
+  int num1 = int.parse(stdin.readLineSync());
+  stdout.write("Enter number 2: ");
+  int num2 = int.parse(stdin.readLineSync());
+  int sum = num1 + num2;
+  print('The Sum of $num1 and $num2 is : $sum');
+}
